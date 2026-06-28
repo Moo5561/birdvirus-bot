2a92922fe98863bb9ed7193556d2e73eb22d2eb7
scoopala7.tngl.sh <me@aesth.lol>
2026-06-27 19:28:16 -0700

    add ping cmd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from dotenv import load_dotenv
-import discord.ext.commands
+import discord.ext.commands as commands
 import os
 
 # do the env variables
@@ -8,12 +8,16 @@ load_dotenv()
 token = os.getenv('KEY')
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.message_content = True
+client = commands.Bot(command_prefix="#", intents=intents)
 
 @client.event
 async def on_ready():
     print(f'the bird has awoken as {client.user}')
 
-client.run(token)
+@commands.command("ping")
+async def ping_cmd(ctx: commands.Context):
+    await ctx.reply("pong :p")
 
-# add more soon idk
+
+client.run(token)
