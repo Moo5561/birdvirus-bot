511830584fbb11815c615629a30c62a160d93b95
alkonsafe <philblakeschool@outlook.com>
2026-06-28 09:17:52 +0700

    add more stuff and log when connect

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import discord
-import dotenv
+from dotenv import load_dotenv
 import discord.ext.commands
 
 # do the env variables
-dotenv.load_dotenv()
+load_dotenv()
+token = os.getenv('KEY')
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('the bird has awoken.')
+
+# add more soon idk
