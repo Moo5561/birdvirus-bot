c337cbca86097069be8d102911f6796ec4c0b17f
mango2man3 <242879590+mango2man3@users.noreply.github.com>
2026-07-02 02:04:23 -0400

    heyo

--- main.py
+++ main.py
@@ -11,18 +11,15 @@ parser = argparse.ArgumentParser(description="run the birdvirus bot")
 parser.add_argument("--host", required=True, help="who is hosting the bot currently")
 args = parser.parse_args()
 
+def get_prefix(bot, message):
+    if bot.user and bot.user.id == 1522117141090799697:
+        return "ht!"
+    return "!"
+
 intents = discord.Intents.default()
 intents.message_content = True
-
-# temp client to get bot id before full init
-temp_client = commands.Bot(command_prefix="!", intents=intents)
-bot_id = 1522117141090799697
-
-# determine prefix
-prefix = "ht!" if bot_id == 1522117141090799697 else "!"
-
 client = commands.Bot(
-    command_prefix=prefix, 
+    command_prefix=get_prefix, 
     intents=intents,
     activity=discord.CustomActivity(name=f"hosted by {args.host}")
 )
