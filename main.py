1c19b71c04bf33288c4c6d59d92f94ec4da34a88
mango2man3 <242879590+mango2man3@users.noreply.github.com>
2026-07-02 02:01:23 -0400

    heyo

--- main.py
+++ main.py
@@ -13,8 +13,16 @@ args = parser.parse_args()
 
 intents = discord.Intents.default()
 intents.message_content = True
+
+# temp client to get bot id before full init
+temp_client = commands.Bot(command_prefix="!", intents=intents)
+bot_id = 1522117141090799697
+
+# determine prefix
+prefix = "ht!" if bot_id == 1522117141090799697 else "!"
+
 client = commands.Bot(
-    command_prefix="!", 
+    command_prefix=prefix, 
     intents=intents,
     activity=discord.CustomActivity(name=f"hosted by {args.host}")
 )
